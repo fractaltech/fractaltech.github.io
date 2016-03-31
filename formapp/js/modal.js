7e087dbdeacc73d1412aa90bde33186f46b8c294
Vivek Goyal <vivekgoyalbhl@gmail.com>
2016-04-01 04:23:41 +0530

    modal resize2

--- formapp/js/modal.js
+++ formapp/js/modal.js
@@ -1,9 +1,9 @@
 
 // Get the modal
-var modal = document.getElementById('myModal');
+var modal = document.getElementById('contactDone');
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+//var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
